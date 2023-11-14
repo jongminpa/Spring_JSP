aae9c6b5485ebab51f523a45c055ae7e6abaafe8
jongminpa <126760210+jongminpa@users.noreply.github.com>
2023-11-14 21:32:29 +0900

    Add files via upload

---
+++ jspboard/jspboard/src/main/java/jspboard/board/jspboard/JspboardApplication.java
@@ -0,0 +1,13 @@
+package jspboard.board.jspboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspboardApplication.class, args);
+	}
+
+}
---
+++ jspboard/jspboard/src/test/java/jspboard/board/jspboard/JspboardApplicationTests.java
@@ -0,0 +1,13 @@
+package jspboard.board.jspboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
